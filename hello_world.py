450711589920f358f04a3fa81fb21b8c346b78f2
Adam Kahoe <89557570+AdamKahoe@users.noreply.github.com>
2022-03-22 15:18:41 -0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,48 @@
+
+print("Hello World!")
+
+# Decimals are called "floats" in Python.
+# Arrays are called "lists" in Python.
+
+name = "John"
+
+print(name)
+
+peepz = ["Jim", "Bob"]
+
+print(peepz[1])
+
+peepz.append("Lou")
+
+print(peepz)
+
+# # loop tests
+
+for x in range(0, 10, 2):
+    print(x)
+    output: 0, 2, 4, 6, 8
+for x in range(5, 1, -3):
+    print(x)
+    output: 5, 2
+
+for(var i = 0; i<5; i++){
+    console.log(i)
+}
+
+for i in range(start, end, incriment):
+    print(i)
+
+#loop ex
+for i in range(0, 5, 1):
+    print(i)
+
+animals =["dolphin", "lion", "fish"]
+
+for i in range(0,len(animals),1):
+    print(animals[i])
+
+def add_my_numbers(num1, num2):
+    results=num1 +num2
+    return results
+
+print(add_my_numbers(5,5))
